ccb3ba6a0d33d5df4990bf8245a502c35d46ba9f
Frederik Bosch <frede.bosch@gmail.com>
2019-12-03 14:23:30 +0100

    fix: change demo to use cdns

---
+++ demo/animation.js
@@ -0,0 +1,37 @@
+const anime = window.anime
+const wrapperEl = document.querySelector('.wrapper')
+const numberOfEls = 200
+const duration = 3000
+const delay = duration / numberOfEls
+
+const tl = anime.timeline({
+  duration: delay,
+  complete: function () { window.requestAnimationFrame(tl.restart) }
+})
+
+function createEl (i) {
+  const el = document.createElement('div')
+  const rotate = (360 / numberOfEls) * i
+  const translateY = -65
+  const hue = Math.round(360 / numberOfEls * i)
+  el.classList.add('el')
+  el.style.backgroundColor = 'hsl(' + hue + ', 80%, 40%)'
+  el.style.transform = 'rotate(' + rotate + 'deg) translateY(' + translateY + '%)'
+  tl.add({
+    begin: function () {
+      anime({
+        targets: el,
+        backgroundColor: ['hsl(' + hue + ', 80%, 60%)', 'hsl(' + hue + ', 80%, 80%)'],
+        rotate: [rotate + 'deg', rotate + 10 + 'deg'],
+        translateY: [translateY + '%', translateY + 10 + '%'],
+        scale: [1, 1.25],
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        duration: duration * 0.1
+      })
+    }
+  })
+  wrapperEl.appendChild(el)
+}
+
+for (let i = 0; i < numberOfEls; i++) createEl(i)
--- demo/index.js
+++ demo/index.js
@@ -3,8 +3,6 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import { connectReducerWorker, connectWorkers, terminateAllWorkers, workerMiddleware } from '../src/redux-webworker-proxy'
 
-import Stats from 'stats.js'
-import anime from 'animejs/lib/anime.es.js'
 import reducer from './reducer'
 
 const initialState = { number: 0, thread: 'none' }
@@ -19,10 +17,7 @@ const worker = () => new Worker('./reducer.worker.js')
 const workerReducer = connectReducerWorker(reducer)(worker, { terminateOnIdle: false })
 const middleware = applyMiddleware(workerMiddleware)
 
-const composedEnhancers = compose(
-  middleware,
-  ...enhancers
-)
+const composedEnhancers = compose(middleware, ...enhancers)
 
 const reducers = compose(connectWorkers)(workerReducer)
 const store = createStore(reducers, initialState, composedEnhancers)
@@ -38,56 +33,4 @@ store.subscribe(() => window.requestAnimationFrame(() => {
   counter.innerText = number + ' \n' + '(' + thread + ')'
 }))
 
-window.setInterval(() => {
-  store.dispatch(sendTest(Math.random() * 10))
-}, 100)
-
-const wrapperEl = document.querySelector('.wrapper')
-const numberOfEls = 200
-const duration = 5000
-const delay = duration / numberOfEls
-
-let tl = anime.timeline({
-  duration: delay,
-  complete: function () { tl.restart() }
-})
-
-function createEl (i) {
-  let el = document.createElement('div')
-  const rotate = (360 / numberOfEls) * i
-  const translateY = -65
-  const hue = Math.round(360 / numberOfEls * i)
-  el.classList.add('el')
-  el.style.backgroundColor = 'hsl(' + hue + ', 80%, 40%)'
-  el.style.transform = 'rotate(' + rotate + 'deg) translateY(' + translateY + '%)'
-  tl.add({
-    begin: function () {
-      anime({
-        targets: el,
-        backgroundColor: ['hsl(' + hue + ', 80%, 60%)', 'hsl(' + hue + ', 80%, 80%)'],
-        rotate: [rotate + 'deg', rotate + 10 + 'deg'],
-        translateY: [translateY + '%', translateY + 10 + '%'],
-        scale: [1, 1.25],
-        easing: 'easeInOutSine',
-        direction: 'alternate',
-        duration: duration * 0.1
-      })
-    }
-  })
-  wrapperEl.appendChild(el)
-};
-
-for (let i = 0; i < numberOfEls; i++) createEl(i)
-
-var stats = new Stats()
-
-document.body.appendChild(stats.dom)
-stats.showPanel(0)
-
-function animate () {
-  stats.begin()
-  stats.end()
-  requestAnimationFrame(animate)
-}
-
-requestAnimationFrame(animate)
+window.setInterval(() => store.dispatch(sendTest(Math.random() * 10)), 100)
